dc2c59ae0ce54fb75bd21953aa4c0f8fea6f6e9e
relust <71765276+relust@users.noreply.github.com>
2024-02-09 16:25:29 +0200

    Delete share/voice_assistant directory

--- share/voice_assistant/change_to_romanian_jarvis_responses.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /share/voice_assistant/
-rm -r /share/voice_assistant/awake_message && cp -r /share/voice_assistant/awake_message_jarvis_ro /share/voice_assistant/awake_message
-cp /share/voice_assistant/no_sound_speech_message/jarvis_no_sound_speech.mp4 /share/voice_assistant/no_sound_speech.mp4
-
-
-
-
-
