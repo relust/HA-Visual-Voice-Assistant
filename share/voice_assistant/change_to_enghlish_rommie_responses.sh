dc26ce58bce9881d0bc71ef77199f001d0363183
Stoleru Ioan Aurel <71765276+relust@users.noreply.github.com>
2024-03-19 10:19:07 +0200

    Delete share directory

--- share/voice_assistant/change_to_enghlish_rommie_responses.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /share/voice_assistant/
-rm -r /share/voice_assistant/awake_message && cp -r /share/voice_assistant/awake_message_rommie_en /share/voice_assistant/awake_message
-cp /share/voice_assistant/no_sound_speech_message/rommie_no_sound_speech.mp4 /share/voice_assistant/no_sound_speech.mp4
-
-
-
-
-
