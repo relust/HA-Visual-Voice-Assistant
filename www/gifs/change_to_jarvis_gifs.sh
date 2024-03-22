1b1b0c29b4137853bc3c2c4aa56cdef725058dce
Stoleru Ioan Aurel <71765276+relust@users.noreply.github.com>
2024-03-22 09:58:53 +0200

    Delete www directory

--- www/gifs/change_to_jarvis_gifs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /config/www/gifs/
-cp /config/www/gifs/jarvis_speech.gif /config/www/gifs/speech.gif
-cp /config/www/gifs/jarvis_listen.gif /config/www/gifs/listen.gif
-
-
-
-
-
--- www/gifs/change_to_roomie_gifs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /config/www/gifs/
-cp /config/www/gifs/roomie_speech.gif /config/www/gifs/speech.gif
-cp /config/www/gifs/roomie_listen.gif /config/www/gifs/listen.gif
-
-
-
